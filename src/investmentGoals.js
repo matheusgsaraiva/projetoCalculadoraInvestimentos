0a2dcb22030536e6aa0fdb1a774ea02ac8e650c3
matheus.gsaraiva@gmail.com <matheus.gsaraiva@gmail.com>
2024-04-15 07:34:31 +0200

    aula 11

---
+++ src/investmentGoals.js
@@ -0,0 +1,12 @@
+function convertToMontlyReturnRate(yearlyReturnRate) {
+  return yearlyReturnRate ** (1 / 12);
+}
+
+function generateReturnsArray(
+  startingAmount = 0,
+  timeHorizon = 0,
+  timePeriod = 'monthly',
+  monthlyContribution = 0,
+  returnRate = 0,
+  returnTimeFrame = 'monthly'
+) {}
