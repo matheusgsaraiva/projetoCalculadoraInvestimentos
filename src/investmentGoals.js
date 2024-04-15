be9c6d7a06a5fa8adaad200c02f073a04e8d161f
matheus.gsaraiva@gmail.com <matheus.gsaraiva@gmail.com>
2024-04-15 07:48:39 +0200

    Formulário de investimento criado e arquivo de projeções criado

--- src/investmentGoals.js
+++ src/investmentGoals.js
@@ -9,4 +9,52 @@ function generateReturnsArray(
   monthlyContribution = 0,
   returnRate = 0,
   returnTimeFrame = 'monthly'
-) {}
+) {
+  // valor 0 é falsy
+  if (!timeHorizon || !startingAmount) {
+    throw new Error(
+      'Investimento inicial e prazo devem ser preenchidos com valores positivos.'
+    );
+  }
+
+  const finalReturnRate =
+    returnTimeFrame === 'monthly'
+      ? 1 + returnRate / 100
+      : convertToMontlyReturnRate(1 + returnRate / 100);
+
+  const finalTimeHorizon =
+    timePeriod === 'monthly' ? timeHorizon : timeHorizon * 12;
+
+  const referenceInvestmentObject = {
+    investedAmount: startingAmount,
+    interestReturns: 0,
+    totalInterestReturns: 0,
+    month: 0,
+    totalAmount: startingAmount,
+  };
+
+  const returnsArray = [referenceInvestmentObject];
+  for (
+    let timeReference = 1;
+    timeReference <= finalTimeHorizon;
+    timeReference++
+  ) {
+    const totalAmount =
+      returnsArray[timeReference - 1].totalAmount * finalReturnRate +
+      monthlyContribution;
+    const interestReturns =
+      returnsArray[timeReference - 1].totalAmount * finalReturnRate;
+    const investedAmount = startingAmount + monthlyContribution * timeReference;
+    const totalInterestReturns = totalAmount - investedAmount;
+    // como os valores das variáveis compartilham o mesmo nome da propriedade não precisamos fazer propriedade: variável
+    returnsArray.push({
+      investedAmount, // propriedade investedAmount
+      interestReturns,
+      totalInterestReturns,
+      month: timeReference,
+      totalAmount,
+    });
+  }
+
+  return returnsArray;
+}
