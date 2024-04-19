dfcb9dc9a48e25c1cba5c95ce49659b8f3f28b37
matheus.gsaraiva@gmail.com <matheus.gsaraiva@gmail.com>
2024-04-19 20:03:17 +0200

    aula 17 - evento de blur e texto em vermelho indicando erro de preenchimento

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 import { generateReturnsArray } from './src/investmentGoals';
 
 const form = document.getElementById('investment-form');
-const calculateButton = document.getElementById('calculate-results');
+// const calculateButton = document.getElementById('calculate-results');
 
 // como a função ta associada a um evento podemos utilizá-la como parametro da própria função
 function renderProgression(evt) {
@@ -11,19 +11,26 @@ function renderProgression(evt) {
 
   //   vamos converter para número pois no formulário o valor vem em formato de texto
   // selecionar e apertar ctrl D para selecionar a próxima aparição do texto selecionado. MUITO ÚTIL
+
   // const startingAmount = Number(
   //   document.getElementById('starting-amount').value
   // );
   // outra forma de selecionar os campos do formulario
-  const startingAmount = Number(form['starting-amount'].value);
+  const startingAmount = Number(
+    form['starting-amount'].value.replace(',', '.')
+  );
   const additionalContribution = Number(
     document.getElementById('additional-contribution').value
   );
   const timeAmount = Number(document.getElementById('time-amount').value);
   const timeAmountPeriod = document.getElementById('time-amount-period').value;
   const returnRatePeriod = document.getElementById('evaluation-period').value;
-  const returnRate = Number(document.getElementById('return-rate').value);
-  const taxRate = Number(document.getElementById('tax-rate').value);
+  const returnRate = Number(
+    document.getElementById('return-rate').value.replace(',', '.')
+  );
+  const taxRate = Number(
+    document.getElementById('tax-rate').value.replace(',', '.')
+  );
 
   const returnsArray = generateReturnsArray(
     startingAmount,
@@ -37,5 +44,42 @@ function renderProgression(evt) {
   console.log(returnsArray);
 }
 
+function validateInput(evt) {
+  // vai printar onde o evento aconteceu
+  console.log(evt.target);
+
+  if (evt.target.value === '') {
+    return;
+  }
+
+  // o {} serve para fazer um destructuring para pegar o pai do elemento onde o evento ocorreu
+  // a desestruturação faz a mesma coisa que isso const parentElement = evt.target.parentElement; nesse caso
+  // vamos atribuir a variável parentElement o pai do elemento onde acontece o evento.
+  // O método parentElement é também uma propriedade do JS para elementos HTML
+  const { parentElement } = evt.target;
+  // pegando o avô do elemento
+  const grandParentElement = evt.target.parentElement.parentElement;
+  const inputValue = evt.target.value.replace(',', '.');
+
+  // vamos colocar o texto em vermelho quando não for um número ou um número negativo
+  if (isNaN(inputValue) || Number(inputValue) <= 0) {
+    const errorTextElement = document.createElement('p'); // <p></p>
+    // atribuindo a classe de cor vermelha do tailwind
+    errorTextElement.classList.add('text-red-500'); // <p class="text-red-500"></p>
+    // gerando o texto do elemento p que acabamos de criar
+    errorTextElement.innerText = 'Insira um valor numérico e maior que zero'; // <p class="text-red-500">Insira um valor numérico e maior que zero</p>
+
+    parentElement.classList.add('error');
+    grandParentElement.appendChild(errorTextElement); // adicionando um filho ao elemento vô
+  }
+}
+
+for (const formElement of form) {
+  // todos os campos do formulário que são obrigatórios tem o atributo name
+  if (formElement.tagName === 'INPUT' && formElement.hasAttribute('name')) {
+    formElement.addEventListener('blur', validateInput);
+  }
+}
+
 // form.addEventListener('submit', renderProgression);
-calculateButton.addEventListener('click', renderProgression);
+form.addEventListener('click', renderProgression);
